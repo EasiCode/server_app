b8c32565d01ad9cb94a1f03971611a2de10cd598
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2022-04-23 17:41:53 +0200

    Data is received using WIFI direct

--- lib/wifi_direct_server.dart
+++ lib/wifi_direct_server.dart
@@ -1,27 +1,45 @@
 // ignore_for_file: avoid_print
 
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:flutter_nearby_connections/flutter_nearby_connections.dart';
 
-late NearbyService nearbyService;
-
 class NearbyServer {
+  // ...........................................................................
   NearbyServer() {
-    init();
-    //onButtonClicked(device);
+    _init();
   }
-  void init() async {
-    nearbyService = NearbyService();
 
-    await nearbyService.init(
-        serviceType: 'mpconn',
-        strategy: Strategy.P2P_CLUSTER,
-        callback: (isRunning) async {
-          if (isRunning) {
-            await nearbyService.stopAdvertisingPeer();
-            //await Future.delayed(const Duration(microseconds: 200));
-            await nearbyService.startAdvertisingPeer();
-            print("advertising Peer...");
-          }
-        });
+  // ...........................................................................
+  void _init() async {
+    _nearbyService = NearbyService();
+    await _advertize();
+    _listenForData();
   }
+
+  // ...........................................................................
+  Future<void> _advertize() async {
+    await _nearbyService.init(
+      serviceType: 'mpconn',
+      strategy: Strategy.P2P_CLUSTER,
+      callback: (isRunning) async {
+        if (isRunning) {
+          await _nearbyService.stopAdvertisingPeer();
+          await _nearbyService.startAdvertisingPeer();
+          print("advertising Peer...");
+        }
+      },
+    );
+  }
+
+  // ...........................................................................
+  _listenForData() {
+    _nearbyService.dataReceivedSubscription(callback: (data) {
+      _nearbyService.sendMessage(data['deviceId'], 'Acknowledgement');
+    });
+  }
+
+  // ...........................................................................
+  late NearbyService _nearbyService;
 }
